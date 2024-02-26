3b94741ff3f2b38284c08b5348efd6f4946143c3
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-02-26 19:27:43 -0300

    refactoring

--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,21 +1,22 @@
 import Player from 'react-player'
-import { useAppSelector } from '../store'
+
+import { useDispatch } from 'react-redux'
+import { next, useCurrentLesson } from '../store/slices/player'
+
 export function Video() {
-  const video = useAppSelector((state) => {
-    const { currentLessonIndex, currentModuleIndex } = state.player
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[
-        currentLessonIndex
-      ]
-    return currentLesson
-  })
+  const dispatch = useDispatch()
+  const { currentLesson } = useCurrentLesson()
+  function handlePlayNext() {
+    dispatch(next())
+  }
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <Player
         width="100%"
         height="100%"
         controls={true}
-        url={`https://www.youtube.com/watch?v=${video.id}`}
+        onEnded={handlePlayNext}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
     </div>
   )
--- src/pages/player.tsx
+++ src/pages/player.tsx
@@ -4,9 +4,17 @@ import { Header } from '../components/header'
 import { Module } from '../components/module'
 import { Video } from '../components/video'
 import { useAppSelector } from '../store'
+import { useCurrentLesson } from '../store/slices/player'
+import { useEffect } from 'react'
 
 export function Player() {
   const modules = useAppSelector((state) => state.player.course.modules)
+
+  const { currentLesson } = useCurrentLesson()
+  useEffect(() => {
+    document.title = `Assistindo: ${currentLesson.title}`
+  }, [currentLesson])
+
   return (
     <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
       <div className="flex w-[1180px] flex-col gap-6">
