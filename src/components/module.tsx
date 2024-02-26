08ea1a0f578aa2bb3078f2f89c03dd0884bb4b55
Lucas Rocha Fernandes <lucasrochaf2018@gmail.com>
2024-02-26 17:53:23 -0300

    add action reducers

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -2,6 +2,8 @@ import { ChevronDown } from 'lucide-react'
 import { Lesson } from './lesson'
 import * as Collapsible from '@radix-ui/react-collapsible'
 import { useAppSelector } from '../store'
+import { useDispatch } from 'react-redux'
+import { play } from '../store/slices/player'
 
 interface ModuleProps {
   title: string
@@ -10,6 +12,7 @@ interface ModuleProps {
 }
 
 export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
+  const dispatch = useDispatch()
   const lessons = useAppSelector(
     (state) => state.player.course.modules[moduleIndex].lessons,
   )
@@ -27,12 +30,13 @@ export function Module({ title, amountOfLessons, moduleIndex }: ModuleProps) {
       </Collapsible.Trigger>
       <Collapsible.Content>
         <nav className="relative flex flex-col gap-4 p-6">
-          {lessons.map((lesson) => {
+          {lessons.map((lesson, index) => {
             return (
               <Lesson
                 key={lesson.id}
                 title={lesson.title}
                 duration={lesson.duration}
+                onPlay={() => dispatch(play([moduleIndex, index]))}
               />
             )
           })}
--- src/components/video.tsx
+++ src/components/video.tsx
@@ -1,12 +1,21 @@
 import Player from 'react-player'
+import { useAppSelector } from '../store'
 export function Video() {
+  const video = useAppSelector((state) => {
+    const { currentLessonIndex, currentModuleIndex } = state.player
+    const currentLesson =
+      state.player.course.modules[currentModuleIndex].lessons[
+        currentLessonIndex
+      ]
+    return currentLesson
+  })
   return (
     <div className="w-full bg-zinc-950 aspect-video">
       <Player
         width="100%"
         height="100%"
         controls={true}
-        url={'https://www.youtube.com/watch?v=YUv2xzvxLT8'}
+        url={`https://www.youtube.com/watch?v=${video.id}`}
       />
     </div>
   )
